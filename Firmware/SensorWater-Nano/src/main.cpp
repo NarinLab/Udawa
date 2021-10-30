98e2ca3120dfbd7103858bab379c7a381b0d1311
iwas108 <64204327+iwas108@users.noreply.github.com>
2021-10-30 14:25:10 +0800

    first trial

--- Firmware/SensorWater-Nano/src/main.cpp
+++ Firmware/SensorWater-Nano/src/main.cpp
@@ -1,9 +1,13 @@
-#include <Arduino.h>
+#include "main.h"
+
+libudawaatmega328 nano;
 
 void setup() {
   // put your setup code here, to run once:
+  nano.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  nano.execute();
 }
---
+++ Firmware/SensorWater-Nano/src/main.h
@@ -0,0 +1,1 @@
+#include <libudawaatmega328.h>
