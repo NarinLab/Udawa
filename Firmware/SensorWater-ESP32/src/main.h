d53f225fd2adb30d75faafd1604e5867759bca24
iwas108 <64204327+iwas108@users.noreply.github.com>
2021-11-13 22:01:08 +0800

    Reverse back to 0.0.1 OTA Major Bug

--- Firmware/SensorWater-ESP32/src/main.h
+++ Firmware/SensorWater-ESP32/src/main.h
@@ -12,7 +12,7 @@
 #include <TaskManagerIO.h>
 
 #define CURRENT_FIRMWARE_TITLE "UDAWA-Damodar"
-#define CURRENT_FIRMWARE_VERSION "0.0.2"
+#define CURRENT_FIRMWARE_VERSION "0.0.1"
 
 const char* settingsPath = "/settings.json";
 
